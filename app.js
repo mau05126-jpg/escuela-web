5fce57e63aa7e097b869ba5aa85e3c83e2d8027e
MauSR <mau05126@gmail.com>
2026-08-05 16:31:17 -0600

    fix: exportar app para Vercel serverless

--- app.js
+++ app.js
@@ -87,3 +87,5 @@ app.post('/avances/eliminar/:id', avanceController.eliminar);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Servidor corriendo en http://localhost:' + PORT));
+
+module.exports = app;
